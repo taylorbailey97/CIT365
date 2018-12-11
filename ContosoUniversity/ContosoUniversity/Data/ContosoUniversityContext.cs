d10c5da01de278a3279cbaf9ff11860c1ee285b6
taylorbailey97 <36019252+taylorbailey97@users.noreply.github.com>
2018-12-11 02:01:35 -0700

    Contoso added

---
+++ ContosoUniversity/ContosoUniversity/Data/ContosoUniversityContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace ContosoUniversity.Models
+{
+    public class ContosoUniversityContext : DbContext
+    {
+        public ContosoUniversityContext (DbContextOptions<ContosoUniversityContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<ContosoUniversity.Models.Student> Student { get; set; }
+    }
+}
